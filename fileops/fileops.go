8d5f33044a4ffc996535e0e105decce6c5bd4111
smnprn <simoneperna8@gmail.com>
2024-05-15 23:53:36 +0200

    fix: fixed image corruption when resizing failed

--- fileops/fileops.go
+++ fileops/fileops.go
@@ -4,10 +4,13 @@ import (
 	"errors"
 	"fmt"
 	"image"
+	"os"
 	"strings"
 
 	"github.com/sunshineplan/imgconv"
 )
+
+var backupFile = "/tmp/backup"
 
 func Convert(oldFileName string, newFileName string, format imgconv.Format) error {
 	src, err := imgconv.Open(oldFileName)
@@ -34,6 +37,11 @@ func Resize(fileName string, resizeMode int, value float64) error {
 		return fmt.Errorf("could not find image format")
 	}
 
+	err = backupImg(src, &imgconv.FormatOption{Format: imgFormat})
+	if err != nil {
+		return errors.New("could not backup file")
+	}
+
 	var resizedImg image.Image
 	switch resizeMode {
 	case 0:
@@ -44,7 +52,33 @@ func Resize(fileName string, resizeMode int, value float64) error {
 
 	err = imgconv.Save(fileName, resizedImg, &imgconv.FormatOption{Format: imgFormat})
 	if err != nil {
+		restoreBackup(fileName, &imgconv.FormatOption{Format: imgFormat})
 		return fmt.Errorf("failed to save image, invalid size")
+	}
+
+	return nil
+}
+
+func backupImg(img image.Image, format *imgconv.FormatOption) error {
+	err := imgconv.Save(backupFile, img, format)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func restoreBackup(fileName string, format *imgconv.FormatOption) error {
+	defer os.Remove(backupFile)
+
+	src, err := imgconv.Open(backupFile)
+	if err != nil {
+		return errors.New("failed to open backup")
+	}
+
+	err = imgconv.Save(fileName, src, format)
+	if err != nil {
+		return errors.New("failed to restore backup")
 	}
 
 	return nil
