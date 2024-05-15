fcab54dcccdb426e43d59c695624e129ed06c8c3
smnprn <simoneperna8@gmail.com>
2024-05-15 17:20:19 +0200

    add: implemented bubbletea ui

--- main.go
+++ main.go
@@ -1,15 +1,147 @@
 package main
 
 import (
+	"log"
+	"strconv"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/smnprn/pixelize/fileops"
 	"github.com/smnprn/pixelize/ui"
+	"github.com/sunshineplan/imgconv"
 )
 
-func main() {
-	operation := ui.HomePage()
+type model struct {
+	homeForm       *huh.Form
+	conversionForm *huh.Form
+	resizeForm     *huh.Form
+	currentPage    *huh.Form
+	width          int
+	height         int
+	style          *Styles
+}
 
-	if operation == 0 {
-		ui.ConversionPage()
-	} else {
-		ui.ResizePage()
+type Styles struct {
+	BorderColor lipgloss.AdaptiveColor
+	Home        lipgloss.Style
+}
+
+func DefaultStyle() *Styles {
+	s := new(Styles)
+
+	s.BorderColor = lipgloss.AdaptiveColor{Light: "#FFFDF5", Dark: "#FFFDF5"}
+	s.Home = lipgloss.
+		NewStyle().
+		BorderForeground(s.BorderColor).
+		BorderStyle(lipgloss.NormalBorder()).
+		Padding(1).
+		Width(80)
+
+	return s
+}
+
+func NewModel() model {
+	style := DefaultStyle()
+	m := model{
+		homeForm:       ui.HomePage(),
+		conversionForm: ui.ConversionPage(),
+		resizeForm:     ui.ResizePage(),
+		style:          style,
+	}
+
+	m.currentPage = m.homeForm
+
+	return m
+}
+
+func (m model) Init() tea.Cmd {
+	return m.homeForm.Init()
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "esc", "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+
+	var cmds []tea.Cmd
+	currentPage, cmd := m.currentPage.Update(msg)
+	if f, ok := currentPage.(*huh.Form); ok {
+		m.currentPage = f
+		cmds = append(cmds, cmd)
+	}
+
+	if m.homeForm.State == huh.StateCompleted {
+		operation := m.homeForm.GetInt("operation")
+
+		switch operation {
+		case 0:
+			m.currentPage = m.conversionForm
+			m.currentPage.Init()
+		case 1:
+			m.currentPage = m.resizeForm
+			m.currentPage.Init()
+		}
+	}
+
+	if m.conversionForm.State == huh.StateCompleted {
+		oldFileName := m.conversionForm.GetString("oldFileName")
+		newFileName := m.conversionForm.GetString("newFileName")
+		format := m.conversionForm.Get("format")
+		confirm := m.conversionForm.GetBool("confirm")
+
+		if confirm {
+			fileops.Convert(oldFileName, newFileName, format.(imgconv.Format))
+		}
+	}
+
+	if m.resizeForm.State == huh.StateCompleted {
+		fileName := m.resizeForm.GetString("fileName")
+		resizeMode := m.resizeForm.GetInt("resizeMode")
+		newSizeStr := m.resizeForm.GetString("newSizeStr")
+		confirm := m.resizeForm.GetBool("confirm")
+
+		if confirm {
+			newSize, err := strconv.ParseFloat(newSizeStr, 64)
+			if err != nil {
+				log.Fatal(err)
+				return m, tea.Quit
+			}
+
+			fileops.Resize(fileName, resizeMode, newSize)
+		}
+	}
+
+	return m, tea.Batch(cmds...)
+}
+
+func (m model) View() string {
+	if m.width == 0 {
+		return "loading..."
+	}
+
+	styledForm := m.style.Home.Render(m.currentPage.View())
+	centeredForm := lipgloss.Place(
+		m.width,
+		m.height,
+		lipgloss.Center,
+		lipgloss.Center,
+		styledForm,
+	)
+
+	return centeredForm
+}
+
+func main() {
+	p := tea.NewProgram(NewModel(), tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
 	}
 }
--- ui/forms.go
+++ ui/forms.go
@@ -1,55 +1,38 @@
 package ui
 
 import (
-	"log"
-	"strconv"
-
 	"github.com/charmbracelet/huh"
-	"github.com/smnprn/pixelize/fileops"
 	"github.com/smnprn/pixelize/utils"
 	"github.com/sunshineplan/imgconv"
 )
 
-func HomePage() int {
-	var operation int
-
+func HomePage() *huh.Form {
 	form := huh.NewForm(
 		huh.NewGroup(
 			huh.NewSelect[int]().
 				Title("Choose an action:").
-				// Description("Lorem ipsum dolor sit amet").
+				Description("Use 'esc' or 'crtl+c' to exit").
 				Options(
 					huh.NewOption("Convert image", utils.CONVERSION),
 					huh.NewOption("Resize image", utils.RESIZE),
 				).
-				Value(&operation),
+				Key("operation"),
 		),
 	)
 
-	err := form.Run()
-	if err != nil {
-		log.Fatal(err)
-		return -1
-	}
-
-	return operation
+	return form
 }
 
-func ConversionPage() {
-	var oldFileName string
-	var newFileName string
-	var format imgconv.Format
-	var confirm bool
-
+func ConversionPage() *huh.Form {
 	form := huh.NewForm(
 		huh.NewGroup(
 			huh.NewInput().
 				Title("Image name or path").
-				Value(&oldFileName),
+				Key("oldFileName"),
 
 			huh.NewInput().
 				Title("New image name").
-				Value(&newFileName),
+				Key("newFileName"),
 
 			huh.NewSelect[imgconv.Format]().
 				Title("New image format").
@@ -61,39 +44,25 @@ func ConversionPage() {
 					huh.NewOption("BMP", imgconv.BMP),
 					huh.NewOption("PDF", imgconv.PDF),
 				).
-				Value(&format),
+				Key("format"),
 
 			huh.NewConfirm().
 				Title("Confirm?").
 				Affirmative("Yes").
 				Negative("No").
-				Value(&confirm),
+				Key("confirm"),
 		),
 	)
 
-	err := form.Run()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	if confirm {
-		fileops.Convert(oldFileName, newFileName, format)
-	}
+	return form
 }
 
-func ResizePage() {
-	var fileName string
-	var resizeMode int
-	var newSizeStr string
-	var newSize float64
-	var confirm bool
-
+func ResizePage() *huh.Form {
 	form := huh.NewForm(
 		huh.NewGroup(
 			huh.NewInput().
 				Title("Image name or path").
-				Value(&fileName),
+				Key("fileName"),
 
 			huh.NewSelect[int]().
 				Title("How do you want to resize the image?").
@@ -102,34 +71,20 @@ func ResizePage() {
 					huh.NewOption("Absolute", 0),
 					huh.NewOption("Percent", 1),
 				).
-				Value(&resizeMode),
+				Key("resizeMode"),
 
 			huh.NewInput().
 				Title("Choose the size").
 				Description("Raw number without symbol (e.g. '%' or 'px')").
-				Value(&newSizeStr),
+				Key("newSizeStr"),
 
 			huh.NewConfirm().
 				Title("Confirm?").
 				Affirmative("Yes").
 				Negative("No").
-				Value(&confirm),
+				Key("confirm"),
 		),
 	)
 
-	err := form.Run()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	if confirm {
-		newSize, err = strconv.ParseFloat(newSizeStr, 64)
-		if err != nil {
-			log.Fatal(err)
-			return
-		}
-
-		fileops.Resize(fileName, resizeMode, newSize)
-	}
+	return form
 }
