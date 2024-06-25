93953d1de43075ccf9c7235801c07a328d4691b2
smnprn <simoneperna8@gmail.com>
2024-06-25 17:56:43 +0200

    ref: improved screen transition

--- ui/model.go
+++ ui/model.go
@@ -11,33 +11,40 @@ import (
 	"github.com/sunshineplan/imgconv"
 )
 
+/*
+* Page indexes:
+* 0 - Home
+* 1 - Conversion
+* 2 - Resize
+ */
+
 type model struct {
-	homeForm       *huh.Form
-	conversionForm *huh.Form
-	resizeForm     *huh.Form
-	currentPage    tea.Model
-	width          int
-	height         int
-	style          *Styles
-	errStatus      error
+	pages            []tea.Model
+	currentPageIndex int
+	completed        bool
+	width            int
+	height           int
+	style            *Styles
+	errStatus        error
 }
 
 func NewModel() model {
 	style := DefaultStyle()
 	m := model{
-		homeForm:       HomePage(),
-		conversionForm: ConversionPage(),
-		resizeForm:     ResizePage(),
-		style:          style,
+		pages: []tea.Model{
+			HomePage(),
+			ConversionPage(),
+			ResizePage(),
+		},
+		currentPageIndex: 0,
+		style:            style,
 	}
-
-	m.currentPage = m.homeForm
 
 	return m
 }
 
 func (m model) Init() tea.Cmd {
-	return m.homeForm.Init()
+	return m.pages[m.currentPageIndex].Init()
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -53,60 +60,14 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	}
 
 	var cmds []tea.Cmd
-	currentPage, cmd := m.currentPage.Update(msg)
-	if f, ok := currentPage.(*huh.Form); ok {
-		m.currentPage = f
-		cmds = append(cmds, cmd)
-	}
+	_, cmd := m.pages[m.currentPageIndex].Update(msg)
+	cmds = append(cmds, cmd)
 
-	if m.homeForm.State == huh.StateCompleted {
-		operation := m.homeForm.GetInt("operation")
-
-		switch operation {
-		case 0:
-			m.currentPage = m.conversionForm
-			m.currentPage.Init()
-		case 1:
-			m.currentPage = m.resizeForm
-			m.currentPage.Init()
+	if _, ok := m.pages[m.currentPageIndex].(*huh.Form); ok {
+		if m.pages[m.currentPageIndex].(*huh.Form).State == huh.StateCompleted {
+			m.ChangePageOperation(m.currentPageIndex)
+			m.pages[m.currentPageIndex].Init()
 		}
-	}
-
-	if m.conversionForm.State == huh.StateCompleted {
-		oldFileName := m.conversionForm.GetString("oldFileName")
-		newFileName := m.conversionForm.GetString("newFileName")
-		format := m.conversionForm.Get("format")
-		confirm := m.conversionForm.GetBool("confirm")
-
-		if confirm {
-			m.errStatus = fileops.Convert(oldFileName, newFileName, format.(imgconv.Format))
-			if m.errStatus != nil {
-				m.currentPage = nil
-			}
-		}
-
-		m.currentPage = nil
-	}
-
-	if m.resizeForm.State == huh.StateCompleted {
-		fileName := m.resizeForm.GetString("fileName")
-		resizeMode := m.resizeForm.GetInt("resizeMode")
-		newSizeStr := m.resizeForm.GetString("newSizeStr")
-		confirm := m.resizeForm.GetBool("confirm")
-
-		if confirm {
-			newSize, err := strconv.ParseFloat(newSizeStr, 64)
-			if err != nil {
-				m.errStatus = errors.New("could not parse new size")
-			}
-
-			m.errStatus = fileops.Resize(fileName, resizeMode, newSize)
-			if m.errStatus != nil {
-				m.currentPage = nil
-			}
-		}
-
-		m.currentPage = nil
 	}
 
 	return m, tea.Batch(cmds...)
@@ -114,10 +75,10 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m model) View() string {
 	var styledForm string
-	if m.currentPage != nil {
-		styledForm = m.style.Framed.Render(m.currentPage.View())
-	} else {
+	if m.completed {
 		styledForm = m.style.Framed.Render(CreateResultScreen(m))
+	} else {
+		styledForm = m.style.Framed.Render(m.pages[m.currentPageIndex].View())
 	}
 
 	centeredForm := lipgloss.Place(
@@ -129,4 +90,58 @@ func (m model) View() string {
 	)
 
 	return centeredForm
+}
+
+func (m *model) ChangePageOperation(pageIndex int) error {
+	switch pageIndex {
+	case 0:
+		homeForm := m.pages[m.currentPageIndex].(*huh.Form)
+		operation := homeForm.GetInt("operation")
+
+		switch operation {
+		case 0:
+			m.currentPageIndex = 1
+		case 1:
+			m.currentPageIndex = 2
+		}
+	case 1:
+		conversionForm := m.pages[m.currentPageIndex].(*huh.Form)
+
+		oldFileName := conversionForm.GetString("oldFileName")
+		newFileName := conversionForm.GetString("newFileName")
+		format := conversionForm.Get("format")
+		confirm := conversionForm.GetBool("confirm")
+
+		if confirm {
+			m.errStatus = fileops.Convert(oldFileName, newFileName, format.(imgconv.Format))
+			if m.errStatus != nil {
+				m.completed = true
+			}
+		}
+
+		m.completed = true
+	case 2:
+		resizeForm := m.pages[m.currentPageIndex].(*huh.Form)
+
+		fileName := resizeForm.GetString("fileName")
+		resizeMode := resizeForm.GetInt("resizeMode")
+		newSizeStr := resizeForm.GetString("newSizeStr")
+		confirm := resizeForm.GetBool("confirm")
+
+		if confirm {
+			newSize, err := strconv.ParseFloat(newSizeStr, 64)
+			if err != nil {
+				m.errStatus = errors.New("could not parse new size")
+			}
+
+			m.errStatus = fileops.Resize(fileName, resizeMode, newSize)
+			if m.errStatus != nil {
+				m.completed = true
+			}
+		}
+
+		m.completed = true
+	}
+
+	return nil
 }
